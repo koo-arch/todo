369c6eb3988bddf4967649e1b78bfeffb333e7d0
koo-arch <yukshw.ko.0623@gmail.com>
2023-06-02 09:50:08 +0900

    update urls.py

--- backend/todo/urls.py
+++ backend/todo/urls.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 from .views import AuthRegister, AuthInfoGetView, UserList, TaskViewSet, FinishedViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 router = DefaultRouter()
@@ -11,6 +12,8 @@ router.register(r'myinfo', AuthInfoGetView)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("token/", TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path("token/refresh", TokenRefreshView.as_view(), name='token_refresh'),
     path("register/", AuthRegister.as_view()),
     path("userlist/", UserList.as_view()),
 ]
