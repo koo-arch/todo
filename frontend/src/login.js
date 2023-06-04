fa4d3f6fcfb4af3ef06007e2413a90d98a6b6e3b
koo-arch <yukshw.ko.0623@gmail.com>
2023-06-04 20:49:42 +0900

    Update login.js

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -3,12 +3,48 @@ import { useCookies } from 'react-cookie';
 import { useForm } from 'react-hook-form';
 import { useHistory } from 'react-router-dom';
 import { axios } from './api/axios.js';
-import { request } from './api/request.js';
+import { requests } from './api/requests.js';
 
-const login = () => {
+const login = (props) => {
+    const history = useHistory();
 
+    const [cookies, setCookie] = useCookies();
+    const { register, handleSubmit, watch, errors } = useForm();
+
+    const getJwt = async(data) => {
+        console.log(data)
+        await axios.post(requests.Login,
+            {
+                email: data.email,
+                password: data.password,
+            },
+        )
+        .then(function (response) {
+            console.log(response.data.access)
+            setCookie('accesstoken', response.data.access, { path: '/' }, { httpOnly: true })
+            setCookie('refreshtoken', response.data.refresh, { path: '/' }, { httpOnly: true })
+            history.push('/');
+        })
+        .catch(err => {
+            console.log("miss")
+            alert("Emailかパスワードが違います");
+        });
+    };
     return (
-        <></>
+        <div className="top-wrapper">
+            <div class="login">
+                <h3>Login</h3>
+            </div>
+            <div class="login-block">
+                <form onSubmit={handleSubmit(getJwt)}>
+                    <label for="email">Email：</label>
+                    <input className='form-control' {...register('email')} />
+                    <label for="password">PassWord：</label>
+                    <input className='form-control' type="password" {...register('password', { required: true })} />
+                    <input className='btn btn-secondary' type="submit" value="ログイン" />
+                </form>
+            </div>
+        </div>
     );
 };
 
