164337fbe0828b26f644248e492d416285a22b07
koo-arch <yukshw.ko.0623@gmail.com>
2023-06-07 22:03:30 +0900

    Create logout.js

---
+++ frontend/src/logout.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
+
+const Logout = (props) => {
+    const navigation = useNavigate();
+    const [cookies, setCookie, removeCookie] = useCookies();
+
+    const removeJWT = () => {
+        removeCookie('accesstoken', { path: '/todo'}, {httpOnly: true});
+        removeCookie('refreshtoken', { path: '/todo'}, {httpOnly: true});
+        navigation('/');
+        console.log('ログアウト');
+        console.log(cookies);
+    }
+    return (
+        <div>
+            <button onClick={removeJWT}>ログアウト</button>
+        </div>
+    )
+}
+
+export default Logout;
--- frontend/src/todo.js
+++ frontend/src/todo.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logout from './logout';
+import Cookies from 'universal-cookie';
 
 const todo = () => {
+  const cookies = new Cookies();
+  console.log(cookies.get('accesstoken'))
+  console.log(cookies.get('refreshtoken'))
   return (
-    <div>todo</div>
+    <div>
+      <Logout />
+    </div>
   );
 };
 
