9bcfc2a736b0c1e58ca9c8a0500eaec0a4311481
koo-arch <yukshw.ko.0623@gmail.com>
2023-07-04 00:01:35 +0900

    setting MUI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
-import Top from './top.js';
-import Register from './auth/register.js';
-import Todo from './todo/todo.js';
+import Top from './top';
+import Login from './auth/login';
+import Register from './auth/register';
+import Todo from './todo/todo';
+import NotFound from './notFound';
 
 const App = () => {
   const [cookies, setCookie, removeCookie] = useCookies(['accesstoken', 'refreshtoken']);
@@ -15,8 +17,10 @@ const App = () => {
         <>
           <Routes>
             <Route path='/' element={ !cookiesLength ? <Top/> : <Navigate replace to='/todo'/>}/>
+            <Route path='/login' element={ !cookiesLength ? <Login/> : <Navigate replace to='/todo'/>}/>
             <Route path='/register' element={ !cookiesLength ? <Register/> : <Navigate replace to='/todo'/> }/>
             <Route path='/todo' element={ cookiesLength ? <Todo/> : <Navigate replace to='/'/> }/>
+            <Route path="*" element={<NotFound/>}/>
           </Routes>
         </>
       </BrowserRouter>
--- frontend/src/top.js
+++ frontend/src/top.js
@@ -1,13 +1,47 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Login from './auth/login';
+import { Button, Box, Container, CssBaseline, Grid, Typography } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
-const top = () => {
+const Top = () => {
+    const defaultTheme = createTheme();
     return (
-        <>
-            <Login/>
-        </>
+        <ThemeProvider theme={defaultTheme}>
+            <Container component={"main"} maxWidth="xs">
+                <CssBaseline/>
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Typography component={"h1"} variant="h3" sx={{mt: 3}}>
+                        ToDoアプリ
+                    </Typography>
+                    <Button 
+                        variant="contained"
+                        fullWidth
+                        href="register"
+                        size="large"
+                        sx={{ mt: 3, mb: 2}}
+                    >
+                        新規登録
+                    </Button>
+                    <Button
+                        variant="outlined"
+                        fullWidth
+                        href="login"
+                        size="large"
+                        sx={{mb: 2 }}
+                    >
+                        ログイン
+                    </Button>
+                </Box>
+
+            </Container>
+        </ThemeProvider>
     );
 };
 
-export default top;
+export default Top;
