0eb28c9b91f39f0447988dd1bc420e068f75f23a
koo-arch <yukshw.ko.0623@gmail.com>
2023-07-07 14:17:15 +0900

    setting validation

--- frontend/src/auth/register.js
+++ frontend/src/auth/register.js
@@ -1,16 +1,30 @@
 import React, {useState, useEffect} from "react";
 import { useCookies } from "react-cookie";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { requestAPI, requestData } from "../api/requests";
 import urls from "../api/urls";
+import { 
+    Button,
+    Box,
+    Container,
+    CssBaseline,
+    Typography,
+    TextField,
+    Avatar,
+    Grid,
+    Link
+} from "@mui/material";
+import HowToRegOutlinedIcon from '@mui/icons-material/HowToRegOutlined';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
 
 const Register = () => {
     const navigation = useNavigate();
-    const [movie, setMoive] = useState([]);
     const [cookie, setCookie] = useCookies(['accesstoken', 'refreshtoken']);
-    const { register, handleSubmit, watch, getValues, errors } = useForm();
+    const { register, handleSubmit, getValues, clearErrors, setError, formState: { errors }, } = useForm();
+    const defaultTheme = createTheme();
+
 
     const postRegister = (data) => {
         const requestJson = new requestData(data);
@@ -24,42 +38,102 @@ const Register = () => {
         const request = new requestAPI(param);
         return request.post();
     }
+
     
     const onSubmit = (data) => {
+        clearErrors();
         postRegister(data)
             .then(res =>  {
                 console.log(res.data)
                 alert('登録が完了しました');
-                navigation('/');
+                navigation('/login');
             })
             .catch(err => {
-                console.log(err.res.data);
-                setMoive(err.res.data)
+                const errRes = err.response.data
+                console.log(errRes);
+                Object.keys(errRes).map((key) => {
+                    setError(`${key}`, { type: "validate", message: errRes[`${key}`]})
+                })
             });
     };
     return(
-        <div>
-            <div>
-                <h3>Register</h3>
-            </div>
-            <div>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <label>Email：</label>
-                    <input {...register('email')} />
-                    <label>PassWord：</label>
-                    <input type="password" {...register('password', { required: true })} />
-                    <label>PassWord（再入力）：</label>
-                    <input type="password" {...register('password_confirmation', { 
-                        required: "パスワードを再入力してください" ,
-                        validate: (value) => {
-                            return(
-                                value === getValues("password") || "パスワードが一致しません"
-                            )
-                        }})} />
-                    <input type="submit" value="登録" />
-                </form>
-            </div>
-        </div>
+        <ThemeProvider theme={defaultTheme}>
+            <Container component={"main"} maxWidth="xs">
+                <CssBaseline/>
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+                        <HowToRegOutlinedIcon/>
+                    </Avatar>
+                    <Typography component={"h1"} variant='h5'>
+                        新規登録
+                    </Typography>
+                    <Box component={"form"} onSubmit={handleSubmit(onSubmit)}>
+                        <TextField
+                            required
+                            error={!!errors.email}
+                            margin="normal"
+                            fullWidth
+                            label="メールアドレス"
+                            type="email"
+                            helperText={!!errors.email && errors.email.message}
+                            {...register('email', { required: "メールアドレスを入力してください" })}
+                        />
+                        <TextField
+                            required
+                            error={!!errors.password}
+                            margin="normal"
+                            fullWidth
+                            label="パスワード"
+                            type="password"
+                            helperText={!!errors.password && errors.password.message}
+                            {...register('password', {
+                                required: "パスワードを入力してください",
+                                minLength: { value: 8, message: `8文字以上で入力してください。` },})
+                            }
+                        />
+                        <TextField
+                            required
+                            error={!!errors.passwordConfirmation}
+                            margin="normal"
+                            fullWidth
+                            label="パスワード(確認)"
+                            type="password"
+                            helperText={!!errors.passwordConfirmation && errors.passwordConfirmation.message}
+                            {...register('passwordConfirmation', {
+                                required: "パスワードを再入力してください",
+                                validate: (value) => {
+                                    return (
+                                        value === getValues('password') || "パスワードが一致しません"
+                                    )}
+                                })
+                            }
+                            />
+                        <Button
+                            variant="contained"
+                            fullWidth
+                            type="submit"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            登録
+                        </Button>
+                    </Box>
+                    <Grid container justifyContent="flex-end">
+                        <Grid item>
+                            <Link href="login" variant="body2">
+                                アカウントをお持ちの場合
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Container>
+        </ThemeProvider>
     );
 };
 
